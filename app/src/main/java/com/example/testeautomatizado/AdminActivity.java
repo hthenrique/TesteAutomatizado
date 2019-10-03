685f1f8080d1b333ed613ce36fa9c1f7cddae853
Henrique Teixeira <48535485+hthenrique@users.noreply.github.com>
2019-10-03 11:35:44 -0300

    novos botões

--- app/src/main/java/com/example/testeautomatizado/AdminActivity.java
+++ app/src/main/java/com/example/testeautomatizado/AdminActivity.java
@@ -1,8 +1,11 @@
 package com.example.testeautomatizado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent home = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(home);
+                break;
+                default:break;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/testeautomatizado/Main2Activity.java
+++ app/src/main/java/com/example/testeautomatizado/Main2Activity.java
@@ -1,8 +1,11 @@
 package com.example.testeautomatizado;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+                break;
+                default:break;
+        }
+        return true;
     }
 }
